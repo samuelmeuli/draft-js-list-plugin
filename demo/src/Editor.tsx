d0f48e54663725bb971904b9cb0126887a9ea129
Samuel Meuli <me@samuelmeuli.com>
2019-08-27 21:32:08 +0200

    Add demo

---
+++ demo/src/Editor.tsx
@@ -0,0 +1,43 @@
+import { DraftEditorCommand, DraftHandleValue, EditorState, RichUtils } from "draft-js";
+import PluginEditor from "draft-js-plugins-editor";
+import React, { Component, ReactElement } from "react";
+
+import createQuickListPlugin from "../../src/index";
+
+const quickListPlugin = createQuickListPlugin();
+const plugins = [quickListPlugin];
+
+interface State {
+	editorState: EditorState;
+}
+
+export default class Editor extends Component<{}, State> {
+	state: Readonly<State> = {
+		editorState: EditorState.createEmpty(),
+	};
+
+	onChange = (editorState: EditorState): void => {
+		this.setState({ editorState });
+	};
+
+	handleKeyCommand = (command: DraftEditorCommand, editorState: EditorState): DraftHandleValue => {
+		const newState = RichUtils.handleKeyCommand(editorState, command);
+		if (newState) {
+			this.onChange(newState);
+			return "handled";
+		}
+		return "not-handled";
+	};
+
+	render = (): ReactElement => {
+		const { editorState } = this.state;
+		return (
+			<PluginEditor
+				editorState={editorState}
+				handleKeyCommand={this.handleKeyCommand}
+				onChange={this.onChange}
+				plugins={plugins}
+			/>
+		);
+	};
+}
---
+++ demo/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Editor from "./Editor";
+
+ReactDOM.render(<Editor />, document.getElementById("root"));
